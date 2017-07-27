3e0241ab3fd31a2e0f0f26fc8687013bae85154f
WillForan <willforan@gmail.com>
2017-07-27 16:34:45 -0400

    working build

--- getAndPackage.bash
+++ getAndPackage.bash
@@ -1,4 +1,7 @@
-curl -L 'https://www.nitrc.org/frs/download.php/5996/ROBEXv12.source.all_platforms.zip//?i_agree=1&download_now=1' | unzip 
+curl -L 'https://www.nitrc.org/frs/download.php/5996/ROBEXv12.source.all_platforms.zip//?i_agree=1&download_now=1' > robex-1.12.zip
+unzip robex-1.12.zip
 mv ROBEX/* robex_1.12/
+tar cJvf robex-1.12.orig.tar.xv --exclude="*/debian"  robex_1.12/ 
+
 cd robex_1.12/
 dpkg-buildpackage -rfakeroot -us -uc -B
