1060469108d17e97f856855825c2d2cd27d5f4b3
WillForan <willforan@gmail.com>
2017-07-28 09:31:19 -0400

    better script; add changelog

--- getAndPackage.bash
+++ getAndPackage.bash
@@ -1,7 +1,17 @@
+#!/usr/bin/env bash
+msg() { echo "## [$(date +%FT%H:%M)] $@"; }
+
+msg "getting robex source (zip) from nitric.org"
 curl -L 'https://www.nitrc.org/frs/download.php/5996/ROBEXv12.source.all_platforms.zip//?i_agree=1&download_now=1' > robex-1.12.zip
 unzip robex-1.12.zip
 mv ROBEX/* robex_1.12/
-tar cJvf robex-1.12.orig.tar.xv --exclude="*/debian"  robex_1.12/ 
 
+msg "making .orig.tar.xz"
+tar cJvf robex-1.12.orig.tar.xz --exclude="*/debian"  robex_1.12/ 
+
+
+msg "building and packaging (dkpg-buildpackage)"
 cd robex_1.12/
 dpkg-buildpackage -rfakeroot -us -uc -B
+
+msg "finished"
